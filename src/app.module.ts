526a7fc07512acfb9cf255172714a2593c980ff7
Nestor <nacastroc@gmail.com>
2023-08-20 14:51:25 -0400

    Limit middleware to GET requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { SectionsModule } from './sections/sections.module';
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { QueryMiddleware } from './_core/middlewares/query.middleware';
-import { SectionsController } from './sections/sections.controller';
 
 @Module({
   imports: [
@@ -28,6 +27,11 @@ import { SectionsController } from './sections/sections.controller';
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(QueryMiddleware).forRoutes(SectionsController); // TODO: make more restricted and add books
+    consumer
+      .apply(QueryMiddleware)
+      .forRoutes(
+        { path: 'sections', method: RequestMethod.GET },
+        { path: 'books', method: RequestMethod.GET },
+      );
   }
 }
